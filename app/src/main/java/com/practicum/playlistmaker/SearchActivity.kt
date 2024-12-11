753e9cd42cc9628875b736c6f7eaa44ad6d70b8b
STer <igorrozembaum@yandex.ru>
2024-12-11 20:32:56 +0300

    Шестой коммит: Реализована логика сохранение значения из поискового поля ввода — EditText при уничтожении activity

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -12,12 +12,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var inputEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val inputEditText = findViewById<EditText>(R.id.searchEditText)
+        inputEditText = findViewById(R.id.searchEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
         toolbar.setNavigationOnClickListener {
             finish()
@@ -31,20 +35,20 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
             }
 
-
-            override fun afterTextChanged(s: Editable?) {
-                // empty
-            }
+            override fun afterTextChanged(s: Editable?) {}
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        if (savedInstanceState != null) {
+            val restoredText = savedInstanceState.getString("search_text")
+            inputEditText.setText(restoredText)
+        }
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -53,5 +57,16 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("search_text", inputEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredText = savedInstanceState.getString("search_text")
+        inputEditText.setText(restoredText)
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.appcompat.app.AppCompatActivity
 import com.practicum.playlistmaker.databinding.ActivitySettingsBinding
 
@@ -19,26 +20,28 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         binding.share.setOnClickListener {
-            val link = "https://practicum.yandex.ru/profile/android-developer/"
+            val linkPracticum = getString(R.string.link_to_the_practicum)
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.type = "text/plain"
-            shareIntent.putExtra(Intent.EXTRA_TEXT, link)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, linkPracticum)
             startActivity(Intent.createChooser(shareIntent, "Отправить с помощью..."))
         }
 
         binding.support.setOnClickListener {
-            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
-            val messageSubject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = getString(R.string.support_message)
+            val messageSubject = getString(R.string.support_subject)
+            val supportEmail = getString(R.string.support_email)
             val supportIntent = Intent(Intent.ACTION_SENDTO)
-            supportIntent.data = Uri.parse("mailto:igorrozembaum@yandex.ru")
+            supportIntent.data = Uri.parse("mailto:$supportEmail")
             supportIntent.putExtra(Intent.EXTRA_SUBJECT, messageSubject)
             supportIntent.putExtra(Intent.EXTRA_TEXT, message)
             startActivity(supportIntent)
         }
 
         binding.userAgreement.setOnClickListener {
+            val linkOffer = getString(R.string.link_to_the_offer)
             val userAgreementIntent =
-                Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+                Intent(Intent.ACTION_VIEW, Uri.parse(linkOffer))
             startActivity(userAgreementIntent)
         }
     }
