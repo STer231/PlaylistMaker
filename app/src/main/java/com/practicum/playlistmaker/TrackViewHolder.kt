9ec890a039d5d4cd03a7bbbec5a4a3e2fac53f7a
STer <igorrozembaum@yandex.ru>
2025-01-16 22:01:38 +0300

    Третий коммит: Реализован RecyclerView Верстка приведена в наджежащий вид

--- app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -1,8 +1,31 @@
 package com.practicum.playlistmaker
 
-import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false)
+) {
+    private val ivCover: ImageView = itemView.findViewById(R.id.ivCover)
+    private val tvTrackTitle: TextView = itemView.findViewById(R.id.tvTrackTitle)
+    private val tvArtistName: TextView = itemView.findViewById(R.id.tvArtistName)
+    private val tvTrackDuration: TextView = itemView.findViewById(R.id.tvTrackDuration)
 
+    fun bind(model: Track) {
+        tvTrackTitle.text = model.trackName
+        tvArtistName.text = model.artistName
+        tvTrackDuration.text = model.trackTime
+        println(model.artworkUrl100)
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .fitCenter()
+            .transform(RoundedCorners(2))
+            .into(ivCover)
+    }
 }
